355c87ab4a400550148e1a80313288936d322778
Olle Lauri Boström <ollebostr@gmail.com>
2018-10-25 21:38:25 +0200

    Adding Droppable component

---
+++ src/components/Droppable.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+type DroppableProps = {
+  onDrop: () => void,
+  onDragStart?: () => void,
+  onDragEnd?: () => void
+};
+
+type DroppableState = {
+  dragging: boolean,
+  draggingTimeout: number
+};
+
+export class Droppable extends Component<DroppableProps, DroppableState> {
+  droppableContainer: any;
+  state = {
+    dragging: false,
+    draggingTimeout: null
+  };
+  static defaultProps = {
+    onDragStart: () => {},
+    onDragEnd: () => {}
+  };
+  componentDidMount() {
+    this.droppableContainer.addEventListener('dragover', this.onDragOver);
+    this.droppableContainer.addEventListener('drop', this.props.onDrop);
+  }
+  componentWillUnmount() {
+    window.clearTimeout(this.state.draggingTimeout);
+    this.droppableContainer.removeEventListener('dragover', this.onDragOver);
+    this.droppableContainer.removeEventListener('drop', this.props.onDrop);
+  }
+  onDragOver = ev => {
+    ev.preventDefault();
+    window.clearTimeout(this.state.draggingTimeout);
+    this.props.onDragStart();
+    const draggingTimeout = window.setTimeout(() => {
+      this.setState({ dragging: false });
+      this.props.onDragEnd();
+    }, 100);
+    this.setState({ dragging: true, draggingTimeout });
+  };
+  render() {
+    return (
+      <div
+        ref={ref => {
+          this.droppableContainer = ref;
+        }}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
---
+++ test/Droppable.spec.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Droppable } from '../src/components/Droppable';
+
+const wait = duration => new Promise(resolve => setTimeout(resolve, duration));
+
+describe('Tests for Droppable', () => {
+  it('invokes onDrop when a drop event fires on the child component', () => {
+    const onDrop = jest.fn();
+    const wrapper = mount(<Droppable onDrop={onDrop}>{<div />}</Droppable>);
+    wrapper.instance().droppableContainer.dispatchEvent(new Event('drop'));
+    expect(onDrop).toHaveBeenCalledTimes(1);
+    wrapper.unmount();
+  });
+  it('invokes onDragStart when a drag is started', () => {
+    const onDragStart = jest.fn();
+    const wrapper = mount(<Droppable onDragStart={onDragStart}>{<div />}</Droppable>);
+    wrapper.instance().droppableContainer.dispatchEvent(new Event('dragover'));
+    expect(onDragStart).toHaveBeenCalledTimes(1);
+    wrapper.unmount();
+  });
+  it('invokes onDragEnd when the drag is ended', async () => {
+    const onDragEnd = jest.fn();
+    const wrapper = mount(<Droppable onDragStart={onDragEnd}>{<div />}</Droppable>);
+    wrapper.instance().droppableContainer.dispatchEvent(new Event('dragover'));
+    await wait(100);
+    expect(onDragEnd).toHaveBeenCalledTimes(1);
+    wrapper.unmount();
+  });
+  it('removes event listeners on unmount', () => {
+    const onDrop = jest.fn();
+    const wrapper = mount(<Droppable onDrop={onDrop}>{<div />}</Droppable>);
+    const droppableContainer = wrapper.instance().droppableContainer;
+    const removeEventListener = jest.spyOn(droppableContainer, 'removeEventListener');
+    wrapper.unmount();
+    expect(removeEventListener.mock.calls[0][0]).toEqual('dragover');
+    expect(removeEventListener.mock.calls[1][0]).toEqual('drop');
+  });
+});
