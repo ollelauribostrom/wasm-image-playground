c4af21de5afece10c0ce5b3f85ee69a68733521d
Olle Lauri Boström <ollebostr@gmail.com>
2018-10-25 20:22:39 +0200

    Adding Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Statusbar from './Statusbar';
+import { Button } from './Button';
+import { Icon } from './Icon';
+
+export function Footer() {
+  return (
+    <div className="footer">
+      <Statusbar />
+      <Button
+        href="https://github.com/ollelauribostrom/wasm-image-playground#credits"
+        target="_blank"
+        rel="noopener noreferrer"
+        label="Credits"
+        customClassName="footer__button"
+      />
+      <Button
+        href="https://github.com/ollelauribostrom/wasm-image-playground"
+        target="_blank"
+        rel="noopener noreferrer"
+        label="View on"
+        icon={<Icon name="github" />}
+        customClassName="footer__button"
+      />
+    </div>
+  );
+}
---
+++ test/Footer.spec.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Footer } from '../src/components/Footer';
+
+describe('Tests for Footer', () => {
+  it('renders correctly', () => {
+    expect(shallow(<Footer />)).toMatchSnapshot();
+  });
+});
