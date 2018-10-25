4c8e45cca8e13c5b83b87405aa7504a450a258b1
Olle Lauri Boström <ollebostr@gmail.com>
2018-10-25 19:59:32 +0200

    Adding Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { Icon } from './Icon';
+import { Button } from './Button';
+import { connect, actions } from '../store/AppStore';
+
+type HeaderProps = {
+  language: string,
+  isLoading: boolean
+};
+
+function Header({ language, isLoading }: HeaderProps) {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <Icon name="image" />
+      </div>
+      <div className="header__toolbar">
+        <Button
+          customClassName="toolbar__button toolbar__button--filter"
+          title="Blur image"
+          icon={<Icon name="blur" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.applyFilter('blur');
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--filter"
+          title="Convert image to grayscale"
+          icon={<Icon name="bw" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.applyFilter('grayscale');
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--filter"
+          title="Sharpen image"
+          icon={<Icon name="sharpen" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.applyFilter('sharpen');
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--filter"
+          title="Invert image"
+          icon={<Icon name="invert" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.applyFilter('invert');
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--filter"
+          title="Apply cooling filter"
+          icon={<Icon name="cool" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.applyFilter('cool');
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--benchmark"
+          title="Open benchmark "
+          icon={<Icon name="benchmark" />}
+          onClick={() => {
+            actions.toggleBenchmarkModal();
+          }}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--action"
+          title="Undo last action"
+          icon={<Icon name="undo" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.undo();
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--action"
+          title="Download"
+          icon={<Icon name="download" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.download();
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--action"
+          title="Delete"
+          icon={<Icon name="delete" />}
+          onClick={async () => {
+            actions.setLoading(true);
+            await actions.delete();
+          }}
+          isDisabled={isLoading}
+        />
+        <Button
+          customClassName="toolbar__button toolbar__button--action"
+          title="Current language mode"
+          icon={<Icon name={language} />}
+          onToggle={() => actions.setLanguage(language === 'js' ? 'wasm' : 'js')}
+          isDisabled={isLoading}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default connect(state => state)(Header);
---
+++ test/Header.spec.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Header from '../src/components/Header';
+import { actions } from '../src/store/AppStore';
+import { Button } from '../src/components/Button';
+import { ToggleHandle } from '../src/components/ToggleHandle';
+
+jest.mock('../src/store/AppStore', () => ({
+  connect: () => arg => arg,
+  actions: {
+    setStatus: jest.fn(),
+    setLoading: jest.fn(),
+    setLanguage: jest.fn(),
+    toggleBenchmarkModal: jest.fn(),
+    applyFilter: jest.fn(),
+    runBenchmark: jest.fn(),
+    undo: jest.fn(),
+    download: jest.fn(),
+    delete: jest.fn()
+  }
+}));
+
+const ButtonIndex = {
+  Blur: 0,
+  Grayscale: 1,
+  Sharpen: 2,
+  Invert: 3,
+  Cool: 4,
+  Benchmark: 5,
+  Undo: 6,
+  Download: 7,
+  Delete: 8
+};
+
+describe('Tests for Header', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(<Header language="js" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('renders correctly when language is wasm', () => {
+    const wrapper = shallow(<Header language="wasm" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('renders correctly when app is loading', () => {
+    const wrapper = shallow(<Header language="js" isLoading />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('handles click on the blur button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Blur)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.applyFilter).toHaveBeenCalledWith('blur');
+  });
+  it('handles click on the grayscale button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Grayscale)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.applyFilter).toHaveBeenCalledWith('grayscale');
+  });
+  it('handles click on the sharpen button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Sharpen)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.applyFilter).toHaveBeenCalledWith('sharpen');
+  });
+  it('handles click on the invert button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Invert)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.applyFilter).toHaveBeenCalledWith('invert');
+  });
+  it('handles click on the cool button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Cool)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.applyFilter).toHaveBeenCalledWith('cool');
+  });
+  it('handles click on the benchmark button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Benchmark)
+      .simulate('click');
+    expect(actions.toggleBenchmarkModal).toHaveBeenCalled();
+  });
+  it('handles click on the undo button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Undo)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.undo).toHaveBeenCalled();
+  });
+  it('handles click on the download button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Download)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.download).toHaveBeenCalled();
+  });
+  it('handles click on the delete button', () => {
+    const wrapper = shallow(<Header language="js" />);
+    wrapper
+      .find(Button)
+      .at(ButtonIndex.Delete)
+      .simulate('click');
+    expect(actions.setLoading).toHaveBeenCalledWith(true);
+    expect(actions.delete).toHaveBeenCalled();
+  });
+  it('handles click on the language toggle handle (js => wasm)', () => {
+    const wrapper = mount(<Header language="js" />);
+    wrapper.find(ToggleHandle).simulate('click');
+    expect(actions.setLanguage).toHaveBeenCalledWith('wasm');
+    wrapper.unmount();
+  });
+  it('handles click on the language toggle handle (wasm => js)', () => {
+    const wrapper = mount(<Header language="wasm" />);
+    wrapper.find(ToggleHandle).simulate('click');
+    expect(actions.setLanguage).toHaveBeenCalledWith('js');
+    wrapper.unmount();
+  });
+});
