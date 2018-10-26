99c6e5751d4a995d2881e1d66277e6f6e68abed0
Olle Lauri Boström <ollebostr@gmail.com>
2018-10-26 12:38:52 +0200

    Adding AppStore + tests

--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -1,23 +1,71 @@
 import createStore from 'react-waterfall';
-import ColorService from '../services/ColorService';
+import ImageService from '../services/ImageService';
+import type { Language, Filter } from '../services/types';
 
-type AppState = {|
-  count: number,
-  color: string
-|};
-
-const initialState: AppState = {
-  count: 0,
-  color: '#fff'
+type AppState = {
+  image?: Image,
+  history: Array<Image>,
+  status?: string,
+  language: Language,
+  isDragging: boolean,
+  isLoading: boolean,
+  showBenchmarkModal: boolean
 };
 
-const config = {
+export const initialState: AppState = {
+  image: null,
+  history: [],
+  status: null,
+  language: 'js',
+  isDragging: false,
+  isLoading: false,
+  showBenchmarkModal: false
+};
+
+export const config = {
   initialState,
   actionsCreators: {
-    click: async ({ count }: AppState): Promise<AppState> => ({
-      count: count + 1,
-      color: await ColorService.getRandomColor()
-    })
+    setStatus: (state: AppState, actions: any, status: boolean): AppState => {
+      return { status };
+    },
+    setLanguage: (state: AppState, actions: any, language: Language): AppState => {
+      return { language };
+    },
+    setLoading: (state: AppState, actions: any, isLoading: boolean): AppState => {
+      return { isLoading };
+    },
+    setDragging: (state: AppState, actions: any, isDragging: boolean): AppState => {
+      return { isDragging };
+    },
+    toggleBenchmarkModal: ({ showBenchmarkModal }: AppState): AppState => {
+      return { showBenchmarkModal: !showBenchmarkModal };
+    },
+    upload: async (state: AppState, actions: any, files: FileList): AppState => {
+      const image = await ImageService.upload(files);
+      return { image, isLoading: false };
+    },
+    applyFilter: async ({ image, history }: AppState, actions: any, filter: Filter): AppState => {
+      const newImage = await ImageService.applyFilter(image, filter);
+      const newHistory = [...history];
+      newHistory.push(image);
+      return { image: newImage, history: newHistory, isLoading: false };
+    },
+    runBenchmark: async (): AppState => {
+      const benchmarkResults = await ImageService.benchmark();
+      return { benchmarkResults, isLoading: false, showBenchmarkModal: true };
+    },
+    undo: ({ history }: AppState): AppState => {
+      const newHistory = [...history];
+      const newImage = newHistory.pop();
+      return { image: newImage, history: newHistory, isLoading: false };
+    },
+    download: ({ image }: AppState): AppState => {
+      ImageService.download(image);
+      return { isLoading: false };
+    },
+    delete: (): AppState => {
+      return { image: null, history: [] };
+    }
   }
 };
 
---
+++ test/store/AppStore.spec.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { config } from '../../src/store/AppStore';
+import ImageService from '../../src/services/ImageService';
+
+jest.mock('../../src/services/ImageService', () => ({
+  applyFilter: jest.fn((image, filter) => {
+    image.appliedFilter = filter;
+    return image;
+  }),
+  benchmark: jest.fn(() => 'results'),
+  upload: jest.fn(images => images[0]),
+  download: jest.fn()
+}));
+
+const actions = config.actionsCreators;
+
+describe('Tests for AppStore', () => {
+  it('has the correct initial state', () => {
+    expect(config.initialState).toMatchSnapshot();
+  });
+  it('handles action: setStatus', () => {
+    const state = { ...config.initialState };
+    const newState = Object.assign({}, state, actions.setStatus(state, {}, 'test-status'));
+    expect(newState.status).toEqual('test-status');
+  });
+  it('handles action: setLoading', () => {
+    const state = { ...config.initialState };
+    const newState = Object.assign({}, state, actions.setLoading(state, {}, true));
+    expect(newState.isLoading).toEqual(true);
+  });
+  it('handles action: setLanguage', () => {
+    const state = { ...config.initialState };
+    const newState = Object.assign({}, state, actions.setLanguage(state, {}, 'wasm'));
+    expect(newState.language).toEqual('wasm');
+  });
+  it('handles action: setDragging', () => {
+    const state = { ...config.initialState };
+    const newState = Object.assign({}, state, actions.setDragging(state, {}, true));
+    expect(newState.isDragging).toEqual(true);
+  });
+  it('handles action: toggleBenchmarkModal', () => {
+    const state = { ...config.initialState };
+    const newState = Object.assign({}, state, actions.toggleBenchmarkModal(state, {}));
+    const finalState = Object.assign({}, newState, actions.toggleBenchmarkModal(newState, {}));
+    expect(newState.showBenchmarkModal).toEqual(true);
+    expect(finalState.showBenchmarkModal).toEqual(false);
+  });
+  it('handles action: upload', async () => {
+    const files = ['fileA', 'fileB'];
+    const state = { ...config.initialState };
+    const newState = await actions.upload(state, {}, files);
+    expect(newState.image).toEqual('fileA');
+    expect(newState.isLoading).toEqual(false);
+  });
+  it('handles action: applyFilter', async () => {
+    const image = {};
+    const state = Object.assign({}, config.initialState, { image, loading: true });
+    const newState = await actions.applyFilter(state, {}, 'BoxBlur');
+    expect(newState.image).toEqual({ appliedFilter: 'BoxBlur' });
+    expect(newState.history).toEqual([image]);
+    expect(newState.isLoading).toEqual(false);
+  });
+  it('handles action: runBenchmark', async () => {
+    const state = { ...config.initialState };
+    const newState = await actions.runBenchmark(state, {});
+    expect(newState.benchmarkResults).toEqual('results');
+    expect(newState.showBenchmarkModal).toEqual(true);
+    expect(newState.isLoading).toEqual(false);
+  });
+  it('handles action: undo', () => {
+    const state = { image: 3, history: [1, 2] };
+    const newState = actions.undo(state, {});
+    expect(newState.image).toEqual(2);
+    expect(newState.history).toEqual([1]);
+    expect(newState.isLoading).toEqual(false);
+  });
+  it('handles action: download', () => {
+    const state = { isLoading: true };
+    const newState = actions.download(state, {});
+    expect(ImageService.download).toHaveBeenCalled();
+    expect(newState.isLoading).toEqual(false);
+  });
+  it('handles action: delete', () => {
+    const state = { image: 3, history: [1, 2] };
+    const newState = actions.delete(state, {});
+    expect(newState).toEqual({ image: null, history: [] });
+  });
+});
