54e50c84ede98b9b7325a8820d033cd1344afbbd
Olle Lauri Boström <ollebostr@gmail.com>
2018-10-26 12:40:52 +0200

    Fixing broken Footer test

--- test/components/Footer.spec.jsx
+++ test/components/Footer.spec.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Footer } from '../../src/components/Footer';
 
+jest.mock('../../src/services/ImageService', () => ({
+  applyFilter: jest.fn(),
+  benchmark: jest.fn(),
+  upload: jest.fn(),
+  download: jest.fn()
+}));
+
 describe('Tests for Footer', () => {
   it('renders correctly', () => {
     expect(shallow(<Footer />)).toMatchSnapshot();
